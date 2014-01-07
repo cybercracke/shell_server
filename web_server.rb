0808db40405247c42b16cc01b2e9cf9b517e74e6
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2014-01-06 19:03:54 -0500

    Lots of ground work on the javascript client

--- web_server.rb
+++ web_server.rb
@@ -71,8 +71,8 @@ class App < Sinatra::Base
       {
         'type' => 'server_list',
         'servers' => [
-          {'name' => 'test1', 'shells' => ['asdf', 'quiten']},
-          {'name' => 'test2', 'shells' => []}
+          {'name' => 'test1', 'shell_keys' => ['asdf', 'quiten'], 'uuid' => SecureRandom.uuid},
+          {'name' => 'test2', 'shell_keys' => [], 'uuid' => SecureRandom.uuid}
         ],
       }
     end
@@ -107,21 +107,119 @@ __END__
 @@ index
 <div id="shell_servers">
 </div>
-<div class="shells">
+<div id="shells">
+  <div id="uuid:asldkjf"><p>Something 1</p></div>
+  <div id="uuid:aldkjf"><p>Something 2</p></div>
+  <div id="uuid:asdkjf"><p>Something 3</p></div>
+  <div id="uuid:aslkjf"><p>Something 4</p></div>
 </div>
 
 <script type="text/javascript">
-  var handleMessage = function(msg) {
-    console.log(msg)
+  // Global for holding the current list of known servers and shells.
+  window.servers = {};
+
+  // Anytime the server list gets updated we'll need to redraw the list. This
+  // function handles it.
+  var drawServers = function() {
+    newServerList = document.createElement('ul');
+
+    for (uuid in window.servers) {
+      serverLi = document.createElement('li');
+      serverLi.innerText = window.servers[uuid]['name'];
+      serverLi.setAttribute('data-uuid', uuid);
+
+      newShellList = document.createElement('ul');
+
+      for (i in window.servers[uuid]['shell_keys']) {
+        shellLi = document.createElement('li');
+        shellLi.innerText = window.servers[uuid]['shell_keys'][i];
+        newShellList.appendChild(shellLi);
+      };
+
+      serverLi.appendChild(newShellList);
+      newServerList.appendChild(serverLi);
+    };
+
+    shellServerElement = document.getElementById('shell_servers');
+    shellServerElement.innerHTML = "";
+    shellServerElement.appendChild(newServerList);
   };
 
+  // The generic message handler for everything coming in from a websocket.
+  var handleMessage = function(msg) {
+    switch(msg.type) {
+      case 'server_list':
+        for (i in msg.servers) { updateServer(msg.servers[i]); };
+        drawServers();
+        break;
+      default:
+        console.log(msg);
+    };
+  };
+
+  // Loop through all shell elements and hide them from being displayed.
+  var hideAllShells = function() {
+    shellElements = document.getElementById('shells').childNodes;
+    for (i in shellElements) {
+      node = shellElements[i];
+
+      if (node.nodeType !== 3 && node.style !== undefined) {
+        node.style.display = 'none';
+      };
+    };
+  };
+
+  // Handles opening new and existing shells. If the shell_id is missing it
+  // assumes a new shell needs to be created, otherwise it'll attempt to
+  // display the existing shell. Due to the initial state required we can't
+  // attempt to rejoin another shell yet, however, shells other than the
+  // current one can simple be hidden.
+  var openShell = function(server_uuid, shell_id) {
+    if (window.servers[server_uuid] !== undefined) {
+      if (shell_id === undefined) {
+        console.log(server_uuid + ':' + shell_id);
+      } else {
+        // Attempt to display a hidden shell
+        showShell(server_uuid, shell_id);
+      }
+    } else {
+      console.log("Attempted to open a shell to a server that doesn't exist.");
+    };
+  };
+
+  // Display an already existing shell.
+  var showShell = function(server, shell_id) {
+    node = document.getElementById(server + ':' + shell_id);
+
+    if (node !== undefined) {
+      // Hide all the shells before displaying a new one.
+      hideAllShells();
+      node.style.display = 'block';
+    } else {
+      console.log("Error: Attempted to display shell that doesn't exist");
+    };
+  };
+
+  // When a change is reported about a server we can use the data provided from
+  // the websocket to update our server list.
+  var updateServer = function(server) {
+    window.servers[server.uuid] = {};
+    window.servers[server.uuid]['name'] = server.name;
+    window.servers[server.uuid]['shell_keys'] = server.shell_keys;
+  };
+
+  // Setup the websocket when the page finishes loading as well as the message
+  // handlers.
   window.onload = function() {
     (function() {
-      var ws       = new WebSocket('ws://' + window.location.host + '/sockets');
+      ws           = new WebSocket('ws://' + window.location.host + '/sockets');
 
       ws.onopen    = function()    { console.log('Websocket opened.'); };
       ws.onclose   = function()    { console.log('Websocket closed.'); };
+      ws.onerror   = function(msg) { console.log(msg); };
       ws.onmessage = function(msg) { handleMessage(JSON.parse(msg.data)); };
+
+      window.ws = ws;
     })();
-  }
+  };
 </script>
