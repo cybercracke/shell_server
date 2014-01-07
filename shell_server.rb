3c7d8553195d14ba7b2254d2e8614196cc51ce11
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2014-01-07 17:25:56 -0500

    Yay got the shell server registering

--- shell_server.rb
+++ shell_server.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'json'
 require 'logger'
 require 'securerandom'
 
@@ -7,6 +8,7 @@ require 'redis'
 require 'hiredis'
 
 UUID = SecureRandom.uuid
+SERVER_NAME = SecureRandom.hex(10)
 
 logger = Logger.new('logs/shell_server.log')
 logger.info("Starting up shell server")
@@ -14,7 +16,7 @@ logger.info("Starting up shell server")
 redis = Redis.new(driver: :hiredis)
 
 # Add this server instance to redis list, TODO expire these in the future.
-redis.sadd('shells:servers', UUID)
+redis.sadd('shells:servers', JSON.generate({'uuid' => UUID, 'name' => SERVER_NAME}))
 redis.publish('shells', JSON.generate({'so' => UUID, 'ty' => 'servers:new', 'da' => ""}))
 
 # Start listening to messages
